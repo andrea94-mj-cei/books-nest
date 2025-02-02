0e1f2a2f1c8306f07c00b7ec4563bb1b643587ef
Andrea Miguel Jarillo <andreamj94@alumnos.cei.es>
2025-02-02 21:59:05 +0100

    commit

---
+++ books-nest/src/lib/routes.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import LandingPage from "../pages/LandingPage";
+import BuscadorLibros from "../pages/BuscadorLibros";
+
+import App from "../App";
+
